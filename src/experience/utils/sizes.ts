52127ef9bf0e4eaae26080e57b8bad2055d1c777
thibautlfr <thibaut.lfr@gmail.com>
2026-06-04 16:56:20 +0200

    feat: add mobile and tablet detection to Sizes class

--- src/experience/utils/sizes.ts
+++ src/experience/utils/sizes.ts
@@ -5,15 +5,22 @@ type SizesEvents = {
 };
 
 export default class Sizes {
+	static readonly MOBILE = 768;
+	static readonly TABLET = 1024;
+
 	width: number;
 	height: number;
 	pixelRatio: number;
+	isMobile: boolean;
+	isTablet: boolean;
 	readonly emitter = mitt<SizesEvents>();
 
 	private handleResize = () => {
 		this.width = window.innerWidth;
 		this.height = window.innerHeight;
 		this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+		this.isMobile = this.width < Sizes.MOBILE;
+		this.isTablet = this.width >= Sizes.MOBILE && this.width < Sizes.TABLET;
 		this.emitter.emit("resize");
 	};
 
@@ -21,6 +28,8 @@ export default class Sizes {
 		this.width = window.innerWidth;
 		this.height = window.innerHeight;
 		this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+		this.isMobile = this.width < Sizes.MOBILE;
+		this.isTablet = this.width >= Sizes.MOBILE && this.width < Sizes.TABLET;
 
 		window.addEventListener("resize", this.handleResize);
 	}
